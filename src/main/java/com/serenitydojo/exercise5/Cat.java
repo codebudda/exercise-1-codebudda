f96fa3fbdb52bb34a99c34d19a7b997ffa070b84
codebudda <33742854+codebudda@users.noreply.github.com>
2022-12-11 11:57:09 -0500

    Creating folder for Exercise 5

---
+++ src/main/java/com/serenitydojo/exercise5/Cat.java
@@ -0,0 +1,76 @@
+package com.serenitydojo.exercise5;
+
+public class Cat {
+    private String name;
+    private String favoriteFood;
+    private int age;
+
+    public static final String CAT_NOISE = "Meow";
+
+    public static String usualFood() {
+        return "Tuna";
+    }
+
+//    public Cat(String name) { //constructor
+//        this.name = name;
+//    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+        //this.favoriteFood = Cat.usualFood();
+        this.favoriteFood = usualFood();
+    }
+
+    public Cat(String name, String favoriteFood, int age) {
+        this.name = name;
+        this.favoriteFood = favoriteFood;
+        this.age = age;
+
+    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//
+//    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setFavoriteFood(String favoriteFood) {
+        this.favoriteFood = favoriteFood;
+    }
+
+    public String getFavoriteFood() {
+        return favoriteFood;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void makeNoise() {
+        //System.out.println("Meow");
+        System.out.println(CAT_NOISE);
+    }
+
+    public void feed(String food) {
+        System.out.println(name + " eats some " + food);
+    }
+
+    public void groom() {
+        lickPaws();
+        cleanFur();
+    }
+
+    private void cleanFur() {
+        System.out.println(name + " cleans his fur");
+    }
+
+    private void lickPaws() {
+        System.out.println(name + " licks his paws");
+    }
+}
+
---
+++ src/test/java/com/serenitydojo/exercise5/WhenCreatingObjects.java
@@ -0,0 +1,60 @@
+package com.serenitydojo.exercise5;
+
+import com.serenitydojo.Cat;
+import com.serenitydojo.Dog;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.serenitydojo.Cat.usualFood;
+
+public class WhenCreatingObjects {
+    @Test
+    public void creating_a_cat() {
+        String name = "Felix";
+        String favoriteFood = "Tuna";
+        int age = 4;
+
+        com.serenitydojo.Cat felix = new com.serenitydojo.Cat("Felix", "Tuna" , 4); //Cat() is a constructor, it is like a special method that creates an object
+
+        //felix.name = "Felix";
+//        felix.setName("Felix");
+        felix.setFavoriteFood("Tuna");
+
+        System.out.println(felix.getName());
+        System.out.println(felix.getFavoriteFood());
+        System.out.println(felix.getAge());
+
+        com.serenitydojo.Cat spot = new com.serenitydojo.Cat("Spot" , "Tuna" , 3);
+//        spot.age = 3;
+        System.out.println("Spot's age is: " + spot.getAge());
+        // We created 2 cat objects with different attribute values
+
+    }
+
+    @Test
+    public void creating_a_dog() {
+        com.serenitydojo.Dog fido = new Dog("Fido" , "Bone" , 5);
+        Assert.assertEquals(fido.getName(), "Fido");
+        Assert.assertEquals(fido.getFavoriteToy(),"Bone");
+        Assert.assertEquals(fido.getAge(),5);
+    }
+
+    @Test
+    public void cat_makes_noise() {
+        //Cat felix = new Cat("Felix" , "Tuna", 4);
+        com.serenitydojo.Cat felix = new com.serenitydojo.Cat("Felix" , 4);
+        com.serenitydojo.Cat spot = new Cat("Spot" , "Salmon", 3);
+
+        //Cat.CAT_NOISE = "LOUD MEOW";
+        felix.makeNoise();
+        felix.feed("Tuna");
+        spot.feed("Salmon");
+        felix.groom();
+        spot.makeNoise();
+        //System.out.println("Usual food = " + Cat.usualFood());
+        //System.out.println("Felix's favorite food = " + felix.getFavoriteFood());
+        System.out.println("Cat's like = " + usualFood());
+    }
+
+
+}
